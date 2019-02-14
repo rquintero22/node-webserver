9e96b5c75f27601a3704f665e90fb4c6eac73426
Raymond Quintero Aparicio <quinteroaparicio@gmail.com>
2019-02-14 11:37:01 -0600

    Probando url

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
      urlDB = 'mongodb://localhost:27017/cafe';
  } else {  
     urlDB = process.env.MONGO_URI;
+    console.log(urlDB);
   }
 
  process.env.urlDB = urlDB;
