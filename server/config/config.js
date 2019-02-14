91098be2abef178bf6a2673779382e0e9f720226
Raymond Quintero Aparicio <quinteroaparicio@gmail.com>
2019-02-14 12:56:16 -0600

    Probando Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,3 @@
-const aws = require('aws-sdk');
 
 /**
  * Puerto
@@ -8,7 +7,7 @@ process.env.PORT = process.env.PORT || 3000;
 /**
  * Entorno
  */
-process.env.NODE_ENV = aws.NODE_ENV || 'dev'; // process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 /**
  * Base de datos
@@ -19,7 +18,7 @@ process.env.NODE_ENV = aws.NODE_ENV || 'dev'; // process.env.NODE_ENV || 'dev';
  if (process.env.NODE_ENV === 'dev') {
      urlDB = 'mongodb://localhost:27017/cafe';
  } else { 
-    urlDB = aws.MONGO_URI; // process.env.MONGO_URI;   
+    urlDB = process.env.MONGO_URI;   
     //urlDB = 'mongodb://cafe-user:cafe123@cluster0-shard-00-00-tegdn.mongodb.net:27017,cluster0-shard-00-01-tegdn.mongodb.net:27017,cluster0-shard-00-02-tegdn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
  }
 
